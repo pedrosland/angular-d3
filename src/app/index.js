19edb35be6b4747fe1010d6f4b652791feaf5171
Pedro Sland <pedro_sland@hotmail.com>
2015-01-11 15:58:50 +0000

    Added d3 service

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('d3Angular', []);
+angular.module('d3Angular', ['d3']);
---
+++ src/components/d3/d3.value.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name d3.d3
+ * @module d3
+ * @description
+ *
+ * Provides directive with d3.js library.
+ */
+angular.module('d3', [])
+  .value('d3', window.d3);
+
+/*
+I prefer this way because I can manage my dependency with Bower without having to copy/paste the library inside a
+module, when updating dependencies it can become forgotten and I don't want to include promises in all of my directives.
+ */
